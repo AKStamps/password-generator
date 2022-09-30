efa30f0057dbd644c1d95a1578d1aed768f9eade
Austin <kennedystampsnc28709@gmail.com>
2022-09-29 21:11:36 -0400

    added blank password length variable to be filled in by function within generate password

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -19,6 +19,7 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","","j","k","l","m","n","o",
 var symbol =  ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+var passwordLength = "";
  
 function generatePassword() {
 
