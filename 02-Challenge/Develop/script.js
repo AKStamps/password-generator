4c9199354354b152e821a45e2c8aa70b6113c0b6
Austin <kennedystampsnc28709@gmail.com>
2022-09-29 21:07:57 -0400

    changed charset array to seperate variables

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -14,74 +14,84 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-var charset = {
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowerCase: "abcdefghijklmnopqrstuvwxyz",
-  symbol: "!@#$%^&*()_+~\\`|}{[]:;?><,./-=",
-  number: "0123456789"
-}
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var symbol =  ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
  
 var chooseCharacter = [
   
   function upperCase() {
-    return charset.upperCase[Math.floor(Math.random() * charset.upperCase.length)];
+    return charset.upperCase[Math.floor(Math.random() * charset.upperCase.length)]
   },
 
   function lowercase() {
-    return charset.lowerCase[Math.floor(Math.random() * charset.lowerCase.length)];
+    return charset.lowerCase[Math.floor(Math.random() * charset.lowerCase.length)]
   },
 
   function symbol() {
-    return charset.symbol[Math.floor(Math.random() * charset.symbol.length)];
+    return charset.symbol[Math.floor(Math.random() * charset.symbol.length)]
   },
 
   function number() {
-    return charset.number[Math.floor(Math.random() * charset.number.length)];
+    return charset.number[Math.floor(Math.random() * charset.number.length)]
   }
 
-];
+]
 
-var confirmLower = confirm("Please click \"OK\" to use Lowercase Letters in your Password");
-if (confirmLower == true) {
-  (lowercase() === confirmLower);
+var confirmLower = confirm("Please click \"OK\" to use Lowercase Letters in your Password")
+if (confirmLower === true) {
+  (lowercase() === confirmLower)
 
-};
-var confirmSymbol = confirm("Please click \"OK\" to use Symbols in your Password");
-if (confirmSymbol == true) {
-  (symbol() === confirmSymbol);
-};
-var confirmNumber = confirm("Please click \"OK\" to use Numbers in your Password");
-if (confirmNumber == true) {
-  (number() === confirmNumber);
-};
-var confirmUpper = confirm("Please click \"OK\" to use Uppercase letters in your Password");
-if (confirmUpper == true) {
-  (upperCase() === confirmUpper);
-};
+}
+var confirmSymbol = confirm("Please click \"OK\" to use Symbols in your Password")
+if (confirmSymbol === true) {
+  (symbol() === confirmSymbol)
+}
+var confirmNumber = confirm("Please click \"OK\" to use Numbers in your Password")
+if (confirmNumber === true) {
+  (number() === confirmNumber)
+}
+var confirmUpper = confirm("Please click \"OK\" to use Uppercase letters in your Password")
+if (confirmUpper === true) {
+  (upperCase() === confirmUpper)
+}
+
+length = prompt("How Many Characters Would You Like Your Password to Be? Minimum of 8 Characters, Maximum of 128")
 
 
 
 function generatePassword() {
 
-length = parseInt(prompt("How Many Characters Would You Like Your Password to Be? Minimum of 8 Characters, Maximum of 128"))
 
-if (!length) {
+function length() {
+
+length;
+
+if (length == '') {
   alert("You Must Enter a Password Length!");
-
-} else if (Number.isInteger(length) == false) {
-  alert("You Must Enter a Numeric Value!");
   
+} 
+  const regex = new RegExp(/[^0-9]/, 'g');
+  const val = length.value
 
-} else if (length < 8 || length >128) {
-  parseInt(alert("Your Password Must Be Between 8 and 128 Characters!"));
+if (val.match(regex)) {
+  alert("Must be a Number!");
+}
 
-} else {
-confirmLower;
-confirmNumber;
-confirmSymbol;
-confirmUpper;
-  
+else if (length < 8 || length > 128) {
+
+  alert("Your Password Must be Between 8 and 128 Characters!");
+
+}
+
 };
+
+
+
+  
+
 
 
 
