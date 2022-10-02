81d89b728420b4340dd53a2b845b33182b649175
Austin <kennedystampsnc28709@gmail.com>
2022-10-02 16:10:14 -0400

    worked with tutor and discovered the error of my ways when using concat and improperly calling the generate password function within the for loop.  It works now!

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -15,8 +15,8 @@ generateBtn.addEventListener("click", writePassword);
 
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a","b","c","d","e","f","g","h","i","","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var symbol =  ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var symbol =  ["!", "%", "&", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 var passwordLength = "";
@@ -38,44 +38,50 @@ function generatePassword() {
   var confirmNumber = confirm("Press OK to use Numbers in your Password");
 
   if (confirmLower === false && confirmNumber === false && confirmSymbol === false && confirmUpper === false) {
-    alert("You Must Choose at Least One Character Type!")
-    return;
+    alert("You Must Choose at Least One Character Type!");
+    var confirmUpper = confirm("Press Ok to use Uppercase Letters in your Password");
+    var confirmLower = confirm("Press OK to use Lowercase Letters in your Password");
+    var confirmSymbol = confirm("Press OK to Use Symbols in your Password");
+    var confirmNumber = confirm("Press OK to use Numbers in your Password");
   }
 
   var choosenCharacters = []
 
   if (confirmLower === true) {
-    choosenCharacters = choosenCharacters.concat(lowerCase);
+    choosenCharacters = [...choosenCharacters, ... lowerCase]
   }
 
   if (confirmUpper === true) {
-    choosenCharacters = choosenCharacters.concat(upperCase);
+    choosenCharacters = [...choosenCharacters, ... upperCase]
   }
 
   if (confirmNumber === true) {
-    choosenCharacters = choosenCharacters.concat(number);
+    choosenCharacters = [...choosenCharacters, ...number]
   }
 
   if (confirmSymbol === true) {
-    choosenCharacters = choosenCharacters.concat(symbol);
+    choosenCharacters = [...choosenCharacters, ...symbol]
   }
 
-  var password = "" 
+  console.log(choosenCharacters);
 
-  for (var i = 0; i < passwordLength; i++) {
-    password = password * choosenCharacters[Math.floor(Math.random() * choosenCharacters.length)];
+  var newPassword = ""
+
+  for (var i = 0; i < passwordLength; i++)  {
+   
+    newPassword = newPassword + choosenCharacters[Math.floor(Math.random() * choosenCharacters.length)];
+
+    
   }
-
-
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  };
-
+  console.log(newPassword);
+  return newPassword;
 };
+
+
+
+
+
+
 
 
 
